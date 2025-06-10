3de7f6a4f83928fe45f93ebad819f6fb73909be1
braheezy <11700503+braheezy@users.noreply.github.com>
2025-06-09 23:02:31 -0700

    Start event loop

--- src/server.zig
+++ src/server.zig
@@ -1,12 +1,33 @@
 const std = @import("std");
+const builtin = @import("builtin");
 const assert = std.debug.assert;
 
 const root = @import("root.zig");
 const readAll = root.readAll;
 const writeAll = root.writeAll;
 const max_msg_size = root.max_msg_size;
+const Conn = @import("Conn.zig").Conn;
+
+var debug_allocator: std.heap.DebugAllocator(.{}) = .init;
 
 pub fn main() !void {
+
+    // Memory allocation setup
+    const allocator, const is_debug = gpa: {
+        if (builtin.os.tag == .wasi) break :gpa .{ std.heap.wasm_allocator, false };
+        break :gpa switch (builtin.mode) {
+            .Debug, .ReleaseSafe => .{ debug_allocator.allocator(), true },
+            .ReleaseFast, .ReleaseSmall => .{ std.heap.smp_allocator, false },
+        };
+    };
+    defer {
+        if (is_debug) {
+            if (debug_allocator.deinit() == .leak) {
+                std.process.exit(1);
+            }
+        }
+    }
+
     const fd = try std.posix.socket(std.posix.AF.INET, std.posix.SOCK.STREAM, 0);
     defer std.posix.close(fd);
 
@@ -31,17 +52,67 @@ pub fn main() !void {
 
     try std.posix.listen(fd, std.os.linux.SOMAXCONN);
 
+    // a map of all client connections, keyed by fd
+    var conns = try allocator.alloc(?Conn, 1024);
+    @memset(conns, null);
+    defer allocator.free(conns);
+
+    // the event loop
+    var poll_args = std.ArrayList(std.os.linux.pollfd).init(allocator);
+    defer poll_args.deinit();
+
     std.log.info("server is running on port 1234", .{});
     while (true) {
-        // accept
-        var client_addr: std.posix.sockaddr = undefined;
-        var client_addr_len: std.posix.socklen_t = @sizeOf(std.posix.sockaddr);
-        const client_fd = try std.posix.accept(fd, &client_addr, &client_addr_len, 0);
-        defer std.posix.close(client_fd);
+        // prepare the arguments of the poll()
+        poll_args.clearAndFree();
+        // put the listening sockets in the first position
+        poll_args.append(.{ .fd = fd, .events = std.os.linux.POLL.IN, .revents = 0 });
 
-        // only serves one client connection at once
-        while (true) {
-            oneRequest(client_fd) catch break;
+        // the rest are connection sockets
+        for (conns) |conn| {
+            const pfd = std.os.linux.pollfd{
+                .fd = conn.fd,
+                .events = std.os.linux.POLL.ERR,
+                .revents = 0,
+            };
+            // poll() flags from the application's intent
+            if (conn.want_read) {
+                pfd.events |= std.os.linux.POLL.IN;
+            }
+            if (conn.want_write) {
+                pfd.events |= std.os.linux.POLL.OUT;
+            }
+            poll_args.append(pfd);
+        }
+
+        // wait for readiness
+        try std.posix.poll(poll_args.items, -1);
+
+        // handle the listening socket
+        if (poll_args.items[0].revents != 0) {
+            if (handleAccept(fd)) |conn| {
+                try conns.append(conn);
+            }
+        }
+
+        // handle connection sockets
+        var i: usize = 1;
+        while (i < poll_args.items.len) {
+            const ready = poll_args.items[i].revents;
+            const conn = conns.items[poll_args.items[i].fd];
+            if (ready & std.os.linux.POLL.IN != 0) {
+                // application logic
+                try handleRead(conn);
+            }
+            if (ready & std.os.linux.POLL.OUT != 0) {
+                // application logic
+                try handleWrite(conn);
+            }
+
+            // close the socket from socket error or application logic
+            if (ready & std.os.linux.POLL.ERR != 0 or conn.want_close) {
+                std.posix.close(conn.fd);
+            }
         }
     }
 }
